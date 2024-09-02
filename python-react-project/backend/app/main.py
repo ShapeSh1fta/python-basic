0002d4707f12a5af5161b70bc6f173615497023e
ShapeSh1fta <alonfux@gmail.com>
2024-09-02 15:05:01 +0000

    removed jinja

--- python-react-project/backend/app/main.py
+++ python-react-project/backend/app/main.py
@@ -1,26 +1,33 @@
 from fastapi import FastAPI, WebSocket, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 
 app = FastAPI()
 
-frontend_static_path = f'{os.getcwd()}/python-react-project/frontend/build'
-# exists = os.path.exists(frontend_static_path)
-# app.mount("/", StaticFiles(directory=frontend_static_path), name="static")
-# templates = Jinja2Templates(directory="/static")
-
-app.mount("/static", StaticFiles(directory=frontend_static_path), name="static")
-templates = Jinja2Templates(directory=f'{os.getcwd()}/python-react-project/backend/templates')
+frontend_build_path = f'../frontend/build'
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return FileResponse(f"{frontend_build_path}/index.html")
 
 @app.post("/api/submit")
 async def submit_form(data: dict):
     return {"message": f"Received: {data['message']}"}
+
+# sets up a health check route. This is used later to show how you can hit
+# the API and the React App url's
+@app.get('/api/health')
+async def health():
+    return { 'status': 'healthy' }
+
+
+# Defines a route handler for `/*` essentially.
+# NOTE: this needs to be the last route defined b/c it's a catch all route
+@app.get("/{rest_of_path:path}")
+async def react_app(req: Request, rest_of_path: str):
+    return FileResponse(f"{frontend_build_path}/index.html")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
